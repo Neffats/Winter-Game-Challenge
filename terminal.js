313bdb0a1891b5804318ab982daee7e1361d5c43
Neffats <36647388+Neffats@users.noreply.github.com>
2018-12-31 15:00:27 +0000

    Added to terminal

--- terminal.js
+++ terminal.js
@@ -8,9 +8,14 @@ class Terminal {
 
 	get_input(){
 		this.currentCommand = this.terminalInput.value;
+		this.terminalInput.value = "";
 		console.log(this.currentCommand);
 	}
 	print_command(){
+		if(this.currentCommand == "clear"){
+			this.terminalWindow.value = "";
+			return;
+		}
 		this.terminalWindow.value += this.currentCommand + "\n";
 	}
 
